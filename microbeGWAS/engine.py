6ffa768b3ae6d39e66f88a90de3e134a500a3f09
prajitrr <151738064+prajitrr@users.noreply.github.com>
2024-03-07 12:37:48 -0800

    Update engine.py

--- microbeGWAS/engine.py
+++ microbeGWAS/engine.py
@@ -43,8 +43,8 @@ def main():
     args = parser.parse_args()
 
     #Call bcftools to process the VCF file and capture the output
-    snp_array = read_np_matrix(subprocess.run(["bcftools query -f '[%GT]\n'"] + [args.vcf]))
-    pos_array = read_np_array(subprocess.run(["bcftools query -f '%POS\n'"] + [args.vcf]))
+    snp_array = read_np_matrix(subprocess.run(["bcftools query -f '[%GT]\n'" + " " + args.vcf]))
+    pos_array = read_np_array(subprocess.run(["bcftools query -f '%POS\n'" + " " + args.vcf]))
 
     #Read in phenotypes
     phen_array = (pd.read_csv(args.phenotype, sep='\t', nrows=length)["Phenotype"])
