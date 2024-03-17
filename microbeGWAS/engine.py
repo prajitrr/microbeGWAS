83ae8bbe695dba1035dc5a9b554859160e6ab826
prajitrr <151738064+prajitrr@users.noreply.github.com>
2024-03-16 21:04:12 -0700

    Update engine.py

--- microbeGWAS/engine.py
+++ microbeGWAS/engine.py
@@ -1,25 +1,11 @@
 #microbeGWAS/microbeGWAS/engine.py
 import numpy as np
-import pandas as pd
+from scipy.stats import t
 
+import csv
 import argparse
 import subprocess
 import sys
-
-from sklearn import linear_model
-from sklearn.metrics import f1_score
-
-def read_np_matrix(path):
-    with open(path, "rb") as f:
-        return np.array(
-            [np.fromiter((chr(c) for c in l.strip()), dtype=np.uint8) for l in f],
-        )
-
-def read_np_array(path):
-    with open(path, "rb") as f:
-        return np.array(
-            [int(l.strip()) for l in f],
-        ).astype(np.uint8)
 
 def row_removal(chunk):
     comparison = chunk[:,1:]
@@ -38,13 +24,13 @@ def main():
     parser.add_argument("positions", help="Text file of SNP positions.", \
                        type=str)
 
-    parser.add_argument("snps", help="Text file of SNPS.", \
+    parser.add_argument("vcf", help="VCF file of SNPs.", \
                        type=str)
   
     parser.add_argument("phenotype", help="Specify TSV file of phenotypes for each sample.", \
                         type=str)
     
-    parser.add_argument('--test', '-t', action='store_true', help='Set the tool to randomly split input data into train and test data (80/20 split).', required=False)
+    parser.add_argument('--prune', '-p', action='store_true', help='Enable minimal LD-pruning.', required=False)
 
     parser.add_argument("-o", "--out", help="Write output to specified text file path. Default: stdout", \
                         metavar="FILE", type=str, required=False, default="stdout")
@@ -55,58 +41,76 @@ def main():
     #snp_array = np.genfromtxt(subprocess.run("bcftools query -f '[%GT]\n'" + " " + args.vcf, shell=True, capture_output=True, text=True).stdout, delimiter=1, dtype=int)
     #pos_array = np.genfromtxt(subprocess.run("bcftools query -f '%POS\n'" + " " + args.vcf, shell=True, capture_output=True, text=True).stdout, delimiter=1, dtype=int)
 
-    pos_array = read_np_array(args.positions)
-    snp_array = read_np_matrix(args.snps)
+    #Define bcftools command
+    bash_command = "bcftools query -f '[%GT]\n' " + args.vcf
+    bash_command_2 = "bcftools query -f '%POS\n' " + args.vcf
 
-    length = len(snp_array[0])
+    #Call bcftools to extract data from VCF file and 
+    process = subprocess.Popen(['bash', '-c', bash_command], stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    snps_string = output.decode()
+    snps = np.array([np.fromiter(((c) for c in l.strip()), dtype=np.uint8) for l in snps_string.split()],)
+    snps = snps.astype(int)
+    x = snps
+
+    process = subprocess.Popen(['bash', '-c', bash_command_2], stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    positions = output.decode()
+    pos_array = np.fromstring(positions, dtype=int, sep='\n')
+    
+    data = np.genfromtxt(args.phenotype, delimiter='\t', skip_header=1, usecols=1, dtype=np.float16)
+    data = data[:len(x.T)]
+    nan_indices = np.where(np.isnan(data))[0]
+    mask = np.ones(data.shape[0], dtype=np.bool_)
+    mask[nan_indices] = False
+    data = data[mask].astype(np.bool_)
+    y = data
+    
+    mask = mask[:len(x.T)]
+    x = x.T[mask].T
+
+    length = len(x[0])
     half_len = np.floor((length)/2)
     
-    #Read in phenotypes
-    phen_array = (pd.read_csv(args.phenotype, sep='\t', nrows=length)["Phenotype"]).values
-    phen_array[np.isnan(phen_array)] = 255
-    phen_array = phen_array.astype(np.uint8)
-    
-
     #Perform LD pruning
-    sums = np.sum(snp_array, axis=1).reshape(-1,1)
-    min_allele = (sums <= half_len).astype(int)
-    sums = sums * np.power(-1, 1 - min_allele) + length * (1 - min_allele)
-    
-    ld_prune_arr = np.concatenate((pos_array.reshape(-1,1),snp_array, sums, min_allele), axis=1)
-    sort_indices = np.argsort(ld_prune_arr[:, length + 1])
-    ld_prune_arr = ld_prune_arr[sort_indices]
-    ld_chunks = np.split(ld_prune_arr, np.where(np.diff(ld_prune_arr[:,length + 1]))[0]+1)
-    
-    pruned_chunks = [row_removal(chunk) for chunk in ld_chunks]
-    pruned_array = np.vstack(pruned_chunks)
+    if args.prune:
+        sums = np.sum(x, axis=1).reshape(-1,1)
+        min_allele = (sums <= half_len).astype(int)
+        sums = sums * np.power(-1, 1 - min_allele) + length * (1 - min_allele)
+        
+        ld_prune_arr = np.concatenate((pos_array.reshape(-1,1),x, sums, min_allele), axis=1)
+        sort_indices = np.argsort(ld_prune_arr[:, length + 1])
+        ld_prune_arr = ld_prune_arr[sort_indices]
+        ld_chunks = np.split(ld_prune_arr, np.where(np.diff(ld_prune_arr[:,length + 1]))[0]+1)
+        
+        pruned_chunks = [row_removal(chunk) for chunk in ld_chunks]
+        pruned_array = np.vstack(pruned_chunks)
+    else:
+        pruned_array = np.concatenate((pos_array.reshape(-1,1),x), axis=1)
     
     #Run GWAS
-    pruned_snp_array = pruned_array[:,1:length].transpose()
-    pruned_snp_array =  np.concatenate((pruned_snp_array, phen_array[:length-1].reshape(-1, 1)), axis=1)
-    drops = pruned_snp_array[:, -1] == 255
-    pruned_snp_array = pruned_snp_array[~drops]
-
-    model = linear_model.LogisticRegression(C=1e40, solver='newton-cg')
+    snp_array = (pruned_array[:,1:]).astype(np.bool_)
+    dof = len(y) - 2
     
-    if not args.test:
-        X = pruned_snp_array[:,:-1]
-        y = pruned_snp_array[:,-1]
-        fitted_model = model.fit(X, y)
-        data = np.column_stack((pruned_array[:,0], fitted_model.coef_[0]))
-        header = "POS\tBETA"
-    else:
-        np.random.shuffle(pruned_snp_array)
-        train = pruned_snp_array[:round(0.8*len(pruned_snp_array))]
-        test = pruned_snp_array[:len(pruned_snp_array) - round(0.8*len(pruned_snp_array))]
-        
-        X=train[:,:-1]
-        y=train[:,-1]
-        fitted_model = model.fit(X, y)
-        
-        f1 = f1_score(test[:,-1],fitted_model.predict(test[:,:-1]))
-        f1_col = np.full((len(pruned_array[:,0]),), f1)
-        data = np.column_stack((pruned_array[:,0], fitted_model.coef_[0], f1_col))
-        header = "POS\tBETA\tF1"
+    n00 = np.sum(~snp_array & ~y.T, axis=1)
+    n01 = np.sum(snp_array & ~y.T, axis=1)
+    n10 = np.sum(~snp_array & y.T, axis=1)
+    n11 = np.sum(snp_array & y.T, axis=1)
+    
+    p0 = n01/(n00 + n01)
+    p1 = n11/(n10 + n11)
+    n0 = n00 + n01
+    n1 = n10 + n11
+
+    beta = np.log((n11*n00)/(n10*n01 + 1e-100)+1e-100)
+    #beta = np.log(n11+1e-100) + np.log(n00+1e-100) - np.log(n10+1e-100) - np.log(n01+1e-100)
+    #alpha = np.log(n01/n00)
+
+    p_vals = 2*(1 - t.cdf(np.abs(np.sqrt((p0*(1-p0)*n0 * p1*(1-p1)*n1)/(p0*(1-p0)*n0 + p1*(1-p1)*n1 + 1e-100))*np.log((p1*(1-p0))/(p0*(1-p1)+1e-100)+1e-100)), dof))
+
+    header = "POS\tBETA\tP_VAL"
+    
+    data = np.column_stack((pruned_array[:,0], beta, p_vals))
 
     if args.out:
         save_to_file_with_header(args.out, data, header)
